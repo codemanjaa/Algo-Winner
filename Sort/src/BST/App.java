7959b07e7b12dc9a5e84aca547b9c0efab09942d
Codemanja <codemanjaa@gmail.com>
2020-07-13 16:02:04 +0200

    add node deletion from BST

--- Sort/src/BST/App.java
+++ Sort/src/BST/App.java
@@ -6,10 +6,13 @@ public class App {
 
         tree.insert(10, "This is ten");
         tree.insert(20, "Twenty");
-        tree.insert(30, "Thirty");
+        tree.insert(15, "Thirty");
         tree.insert(5, "Five");
 
         System.out.println(tree.findMax().key);
         System.out.println(tree.findMin().value);
+
+        System.out.println(tree.delete(10));
+        System.out.println(tree.findMin().key);
     }
 }
--- Sort/src/BST/BST.java
+++ Sort/src/BST/BST.java
@@ -60,4 +60,135 @@ public class BST {
         }
         return last;
     }
+
+    public boolean delete(int key) {
+        Node current = root;
+        Node parent = root;
+        boolean isLeftChild = false;
+
+
+        while (current.key != key) {
+            parent = current;
+
+            if (key < current.key) {
+                isLeftChild = true;
+                current = current.leftChild;
+            } else {
+                current = current.rightChild;
+                isLeftChild = false;
+            }
+            if (current == null) {
+                return false;
+            }
+
+        }
+
+        Node delNode = current;
+
+        //if node is leaf
+
+        if(delNode.leftChild == null && delNode.rightChild == null){
+            if(delNode == root){
+                root = null;
+            }
+            else if(isLeftChild){
+                parent.leftChild = null;
+            }
+            else {
+                parent.rightChild = null;
+            }
+        }
+        else if(delNode.rightChild == null){
+            if(delNode == root){
+                root = delNode.leftChild;
+            }
+            else if(isLeftChild){
+                parent.leftChild = delNode.leftChild;
+            }
+            else {
+                parent.rightChild = delNode.leftChild;
+            }
+        }
+
+        else if(delNode.leftChild == null){
+            if(delNode == root){
+                root = delNode.rightChild;
+            }
+            else if(isLeftChild){
+                parent.leftChild = delNode.rightChild;
+            }
+            else{
+                parent.rightChild = delNode.rightChild;
+            }
+        }
+        else {
+
+            Node successor = getSuccessor(delNode);
+
+            // Connect parent of node todelete to successor instead
+            if(delNode == root){
+                root = successor;
+            }
+            else if(isLeftChild){
+                parent.leftChild = successor;
+            }
+            else{
+                parent.rightChild = successor;
+            }
+            successor.leftChild = delNode.leftChild;
+
+        }
+
+        return true;
+
+    }
+
+    private Node getSuccessor(Node dellNode){
+        Node successorParnet = dellNode;
+        Node successor = dellNode;
+
+        Node current = dellNode.rightChild; // Get the right chiled
+
+        while(current != null){ // start going left down the tree
+            successorParnet=successor;
+            successor = current;
+
+            current = current.leftChild;
+        }
+
+        if(successor != dellNode.rightChild){
+            successorParnet.leftChild = successor.rightChild;
+            successor.rightChild = dellNode.rightChild;
+        }
+        return successor;
+
+    }
+
 }
+
+
+        /*while(current != null){
+
+
+            if(current.key > key){
+                current = current.leftChild;
+                if(current.key == key){
+                    current.leftChild = null;
+                }
+
+            }
+            else {
+                current = current.rightChild;
+                if(current.key == key){
+                    current.rightChild = null;
+                }
+
+
+            }
+        }
+*/
+
+        // node has one child
+
+        // node has two child
+
